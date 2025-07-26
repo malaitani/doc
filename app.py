bcaa2a828eef1c47e8a886c5db3fb16cadaa188a
M Itani <mitani@wustl.edu>
2025-07-26 14:58:01 -0500

    app.py

--- app.py
+++ app.py
@@ -84,5 +84,8 @@ def index():
     return render_template('index.html', schedule=schedule, dates=dates,
                            doctor_input=doctor_input, unavailable_input=unavailable_input)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
